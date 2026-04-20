34b751cb1026a37c043cb0ac984ab83e74d0861f
joba <josefine.backlund@hotmail.com>
2026-04-20 11:58:33 +0200

    Skapat undersidor med komponenter för header och footer, routing för nav.

---
+++ src/app/partials/mainfooter/mainfooter.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-mainfooter',
+  imports: [RouterLink],
+  templateUrl: './mainfooter.html',
+  styleUrl: './mainfooter.css',
+})
+export class Mainfooter {}
