de3276fcdfc7611f682a1d874f22e9dfb0d49ac1
joba <josefine.backlund@hotmail.com>
2026-04-22 10:14:54 +0200

    Konvertera c och f.

--- src/app/pages/converter/converter.ts
+++ src/app/pages/converter/converter.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-converter',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './converter.html',
   styleUrl: './converter.css',
 })
-export class Converter {}
+export class Converter {
+  celsius: string = '';
+  fahrenheit: string = '';
+
+  fToC() {
+    this.convertToCelsius(this.fahrenheit);
+  }
+  CtoF() {
+    this.convertToFahrenheit(this.celsius);
+  }
+
+  convertToFahrenheit(input: string): void {
+    let number: number = Number(input);
+    let calc: number = number * 1.8 + 32;
+    let result: string = calc.toString();
+    this.fahrenheit = result;
+  }
+  convertToCelsius(input: string): void {
+    let number: number = Number(input);
+    let calc: number = (number - 32) / 1.8;
+    let result: string = calc.toString();
+    this.celsius = result;
+  }
+}
--- src/app/partials/mainfooter/mainfooter.ts
+++ src/app/partials/mainfooter/mainfooter.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-mainfooter',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './mainfooter.html',
   styleUrl: './mainfooter.css',
 })
