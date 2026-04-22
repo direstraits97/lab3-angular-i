2b6648f2e80cced1a1090aca72d51310297970c0
joba <josefine.backlund@hotmail.com>
2026-04-22 10:28:11 +0200

    Konvertera m och f.

--- src/app/pages/converter/converter.ts
+++ src/app/pages/converter/converter.ts
@@ -10,6 +10,8 @@ import { FormsModule } from '@angular/forms';
 export class Converter {
   celsius: string = '';
   fahrenheit: string = '';
+  feet: string = '';
+  meter: string = '';
 
   fToC() {
     this.convertToCelsius(this.fahrenheit);
@@ -18,16 +20,35 @@ export class Converter {
     this.convertToFahrenheit(this.celsius);
   }
 
-  convertToFahrenheit(input: string): void {
+  mToF() {
+    this.convertToFeet(this.meter);
+  }
+  fToM() {
+    this.convertToMeter(this.feet);
+  }
+
+  private convertToFahrenheit(input: string): void {
     let number: number = Number(input);
     let calc: number = number * 1.8 + 32;
     let result: string = calc.toString();
     this.fahrenheit = result;
   }
-  convertToCelsius(input: string): void {
+  private convertToCelsius(input: string): void {
     let number: number = Number(input);
     let calc: number = (number - 32) / 1.8;
     let result: string = calc.toString();
     this.celsius = result;
+  }
+  private convertToFeet(input: string): void {
+    let number: number = Number(input);
+    let calc: number = number * 3.28084;
+    let result: string = calc.toString();
+    this.feet = result;
+  }
+  private convertToMeter(input: string): void {
+    let number: number = Number(input);
+    let calc: number = number * 0.3048;
+    let result: string = calc.toString();
+    this.meter = result;
   }
 }
